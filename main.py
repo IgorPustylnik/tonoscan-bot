3053007b0d0eb8306522ef2a9122e714eb20e300
Igor <darthveider36@gmail.com>
2024-05-02 20:48:02 +0300

    fix method

--- main.py
+++ main.py
@@ -1,14 +1,16 @@
 import datetime
-
-import requests
-from flask import Flask, request
-import telebot
-from datetime import datetime
 import json
+import requests
+from aiogram.types import ParseMode
+from flask import Flask, request
+from flask_restful import Api
+import asyncio
+from aiogram import Bot, Dispatcher, executor, types
+
 
 app = Flask(__name__)
-
-bot = telebot.TeleBot('7040913152:AAF08LsRRx4y-jbCN9T8WnAtppFqwrgJCos')
+api = Api(app)
+dp = Dispatcher(Bot(token="7040913152:AAF08LsRRx4y-jbCN9T8WnAtppFqwrgJCos"))
 
 month_names = {
     1: "января",
@@ -32,7 +34,7 @@ def index():
 
 
 def send_to_server(number: int, id: int):
-    url = 'https://tonometer.onrender.com/auth/XXXXXXXXXXXXXXXXXXXXXX'
+    url = 'https://tonometer.onrender.com/auth/add-telegram-id'
 
     headers = {'Content-Type': 'application/json'}
 
@@ -56,7 +58,7 @@ def send_to_server(number: int, id: int):
 def message_request():
     if request.headers['Content-Type'] == 'application/json':
         parsed_json = request.json
-        print("tried")
+        print("tried to parse request")
         print(parsed_json)
         send_message(parsed_json)
         return 'success', 200
@@ -64,28 +66,28 @@ def message_request():
         return 'unsupported media type', 415
 
 
-@bot.message_handler(commands=['start'])
-def start_message(message):
-    markup = telebot.types.ReplyKeyboardMarkup(resize_keyboard=True)
-    button = telebot.types.KeyboardButton("Поделиться контактом", request_contact=True)
+@dp.message_handler(commands=['start'])
+async def start_message(message: types.Message):
+    markup = types.ReplyKeyboardMarkup(resize_keyboard=True)
+    button = types.KeyboardButton("Поделиться контактом", request_contact=True)
     markup.add(button)
-    bot.send_message(message.chat.id,
-                     'Здравствуйте! Нажмите на кнопку "Поделиться контактом", чтобы зарегистрироваться в боте.',
-                     reply_markup=markup)
+    await message.answer('Здравствуйте! Нажмите на кнопку "Поделиться контактом", чтобы зарегистрироваться в боте.',
+                         reply_markup=markup)
 
 
-@bot.message_handler(content_types=['contact'])
-def handle_contact(m):
+@dp.message_handler(content_types=['contact'])
+async def handle_contact(m: types.Message):
     contact = m.contact
-    send_to_server(contact.phone_number, contact.user_id)
-    bot.send_message(m.chat.id, "Вы успешно зарегистрировались!", reply_markup=telebot.types.ReplyKeyboardRemove(),
-                     parse_mode='Markdown')
-    bot.send_message(m.chat.id, "Если вы ещё не внесли свой номер в список для рассылки в мобильном приложении"
-                                "на телефоне вашей бабки, обязательно сделайте это! Это необходимо для того, чтобы бот "
-                                "моментально присылал вам информацию об измерениях давления.")
+    await m.answer("Вы успешно зарегистрировались!", reply_markup=types.ReplyKeyboardRemove(),
+                   parse_mode=ParseMode.MARKDOWN)
+    await m.answer("Если вы ещё не внесли свой номер в список для рассылки в мобильном приложении"
+                   " на телефоне вашей бабки, обязательно сделайте это! Это необходимо для того, чтобы бот "
+                   " моментально присылал вам информацию об измерениях давления.")
+    await send_to_server(contact.phone_number, contact.user_id)
 
 
-def send_message(pjs):
+async def send_message(pjs):
+    from datetime import datetime
     chat_id = pjs['id']
     dia = pjs['info'][0]['dia']
     sys = pjs['info'][1]['sys']
@@ -93,17 +95,24 @@ def send_message(pjs):
     date_info = datetime.fromisoformat(pjs['info'][3]['date'])
     date = date_info.strftime("%d %B %Y года в %H:%M").format(month=month_names[date_info.month])
     name = pjs['info'][4]['name']
-    # image = pjs['image']
-    # image_bytes = bytes(image)
     text = f'Получены данные об измерении, произведённом {date} от {name}:\nDIA: {dia}\nSYS: {sys}\nPULSE: {pulse}\nБудьте здоровы!'
-    bot.send_message(chat_id, text)
+
+    await dp.bot.send_message(chat_id=chat_id, text=text)
 
 
-@bot.message_handler()
-def debug(message):
-    print(f'{datetime.fromtimestamp(message.date)} {message.from_user.username}: {message.text} {message}')
+@dp.message_handler()
+async def debug(message: types.Message):
+    print(f'{message.date} {message.from_user.username}: {message.text}')
 
-bot.polling()
 
-if __name__ == "__main__":
-    app.run(host='0.0.0.0', port=5000, debug=True)
+async def is_enabled():
+    while True:
+        await app.run()
+
+
+async def on_startup():
+    asyncio.create_task(asyncio.to_thread(app.run))
+
+
+if __name__ == '__main__':
+    executor.start_polling(dispatcher=dp, skip_updates=True, on_startup=on_startup)
