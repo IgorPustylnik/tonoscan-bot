a17c814e2e2a3fd59ed84317f034181f65de7674
Igor <darthveider36@gmail.com>
2024-05-03 22:11:38 +0300

    bot remade

--- main.py
+++ main.py
@@ -1,14 +1,126 @@
 import json
-import requests
-from aiogram.types import ParseMode
+from datetime import datetime
+
 from flask import Flask, request
-from flask_restful import Api
-import asyncio
-from aiogram import Bot, Dispatcher, executor, types
+import requests
 
 app = Flask(__name__)
-api = Api(app)
-dp = Dispatcher(Bot(token="7040913152:AAF08LsRRx4y-jbCN9T8WnAtppFqwrgJCos"))
+
+token = "7040913152:AAF08LsRRx4y-jbCN9T8WnAtppFqwrgJCos"
+
+
+def set_webhook():
+    url = f"https://api.telegram.org/bot{token}/setWebhook"
+    webhook_url = "https://tonoscan-bot.onrender.com/webhook"
+    data = {"url": webhook_url}
+    response = requests.post(url, json=data)
+    if response.ok:
+        print("Вебхук установлен успешно.")
+    else:
+        print("Ошибка при установке вебхука:", response.text)
+
+
+@app.route('/webhook', methods=['POST'])
+def webhook_handler():
+    if request.headers['Content-Type'] == 'application/json':
+        update = request.json
+        if 'message' in update:
+            message = update['message']
+            date = datetime.fromtimestamp(message['date'] / 1000.0).isoformat()
+            username = message['from']['username']
+            text = message['text']
+            print(f'{date} {username}: {text}')
+            handle_message(message)
+        return 'ok', 200
+    else:
+        return 'unsupported media type', 415
+
+
+def handle_message(message):
+    if 'text' in message:
+        text = message['text']
+        chat_id = message['chat']['id']
+
+        if text.startswith('/start'):
+            send_message_with_contact_button(chat_id)
+        else:
+            send_message(chat_id, "Извините, у меня слабые социальные навыки, поэтому в ответ на ваши сообщения я "
+                                  "всегда буду присылать вам этот текст.")
+    elif 'contact' in message:
+        contact = message['contact']
+        chat_id = message['chat']['id']
+        handle_contact(chat_id, contact['phone_number'], contact['user_id'])
+
+
+def send_message_with_contact_button(chat_id):
+    url = f"https://api.telegram.org/bot{token}/sendMessage"
+    data = {
+        "chat_id": chat_id,
+        "text": "Здравствуйте! Нажмите на кнопку 'Поделиться контактом', чтобы зарегистрироваться в боте.",
+        "reply_markup": {
+            "keyboard": [
+                [{"text": "Поделиться контактом", "request_contact": True}]
+            ],
+            "resize_keyboard": True
+        }
+    }
+    response = requests.post(url, json=data)
+    if response.ok:
+        print("Сообщение с кнопкой контакта отправлено успешно.")
+    else:
+        print("Ошибка при отправке сообщения:", response.text)
+
+
+def handle_contact(chat_id, phone_number, user_id):
+    send_to_server(phone_number, user_id)
+    url = f"https://api.telegram.org/bot{token}/sendMessage"
+    text1 = "Вы успешно зарегистрировались!"
+    text2 = ("Если вы ещё не внесли свой номер в список для рассылки в мобильном приложении на телефоне вашей бабки, "
+             "обязательно сделайте это! Это необходимо для того, чтобы бот моментально присылал вам информацию об измерениях давления.")
+    data1 = {
+        "chat_id": chat_id,
+        "text": text1,
+        "reply_markup": {
+            "remove_keyboard": True
+        }
+    }
+    data2 = {"chat_id": chat_id, "text": text2}
+    requests.post(url, json=data1)
+    requests.post(url, json=data2)
+
+
+def send_message(chat_id, text):
+    method = "sendMessage"
+    token = "7040913152:AAF08LsRRx4y-jbCN9T8WnAtppFqwrgJCos"
+    url = f"https://api.telegram.org/bot{token}/{method}"
+    data = {"chat_id": chat_id, "text": text}
+    requests.post(url, data=data)
+
+
+@app.route('/send_message', methods=['POST'])
+def message_request():
+    if request.headers['Content-Type'] == 'application/json':
+        parsed_json = request.json
+        print("tried to parse request")
+        print(parsed_json)
+        send_info(parsed_json)
+        return 'success', 200
+    else:
+        return 'unsupported media type', 415
+
+
+def send_info(pjs):
+    from datetime import datetime
+    chat_id = pjs['id']
+    dia = pjs['info'][0]['dia']
+    sys = pjs['info'][1]['sys']
+    pulse = pjs['info'][2]['pulse']
+    date_info = datetime.fromisoformat(pjs['info'][3]['date'])
+    date = date_info.strftime("%d {month} %Y года в %H:%M").format(month=month_names[date_info.month])
+    name = pjs['info'][4]['name']
+    text = f'Получены данные об измерении, произведённом {date} от {name}:\nDIA: {dia}\nSYS: {sys}\nPULSE: {pulse}\nБудьте здоровы!'
+    send_message(chat_id, text)
+
 
 month_names = {
     1: "января",
@@ -24,11 +136,6 @@ month_names = {
     11: "ноября",
     12: "декабря"
 }
-
-
-@app.route('/', methods=['GET'])
-def index():
-    return 'Привет, мир! Это удалённый сервер.'
 
 
 def send_to_server(number: int, id: int):
@@ -52,62 +159,6 @@ def send_to_server(number: int, id: int):
         print('Ошибка при запросе:', response.status_code)
 
 
-@app.route('/send_message', methods=['POST'])
-def message_request():
-    if request.headers['Content-Type'] == 'application/json':
-        parsed_json = request.json
-        print("tried to parse request")
-        print(parsed_json)
-        send_message(parsed_json)
-        return 'success', 200
-    else:
-        return 'unsupported media type', 415
-
-
-@dp.message_handler(commands=['start'])
-async def start_message(message: types.Message):
-    markup = types.ReplyKeyboardMarkup(resize_keyboard=True)
-    button = types.KeyboardButton("Поделиться контактом", request_contact=True)
-    markup.add(button)
-    await message.answer('Здравствуйте! Нажмите на кнопку "Поделиться контактом", чтобы зарегистрироваться в боте.',
-                         reply_markup=markup)
-
-
-@dp.message_handler(content_types=['contact'])
-async def handle_contact(m: types.Message):
-    contact = m.contact
-    await m.answer("Вы успешно зарегистрировались!", reply_markup=types.ReplyKeyboardRemove(),
-                   parse_mode=ParseMode.MARKDOWN)
-    await m.answer("Если вы ещё не внесли свой номер в список для рассылки в мобильном приложении"
-                   " на телефоне вашей бабки, обязательно сделайте это! Это необходимо для того, чтобы бот "
-                   " моментально присылал вам информацию об измерениях давления.")
-    await send_to_server(contact.phone_number, contact.user_id)
-
-
-def send_message(pjs):
-    from datetime import datetime
-    chat_id = pjs['id']
-    dia = pjs['info'][0]['dia']
-    sys = pjs['info'][1]['sys']
-    pulse = pjs['info'][2]['pulse']
-    date_info = datetime.fromisoformat(pjs['info'][3]['date'])
-    date = date_info.strftime("%d %B %Y года в %H:%M").format(month=month_names[date_info.month])
-    name = pjs['info'][4]['name']
-    text = f'Получены данные об измерении, произведённом {date} от {name}:\nDIA: {dia}\nSYS: {sys}\nPULSE: {pulse}\nБудьте здоровы!'
-    dp.bot.send_message(chat_id=chat_id, text=text)
-
-
-@dp.message_handler()
-async def debug(message: types.Message):
-    print(f'{message.date} {message.from_user.username}: {message.text}')
-
-
-async def is_enabled():
-  while True:
-    await app.run()
-
-async def on_startup(x):
-    await asyncio.create_task(asyncio.to_thread(app.run(host='0.0.0.0', port=5000, debug=True)))
-
 if __name__ == '__main__':
-    executor.start_polling(dispatcher=dp, skip_updates=True, on_startup=on_startup)
+    app.run(host='0.0.0.0', port=5000, debug=True)
+    set_webhook()
